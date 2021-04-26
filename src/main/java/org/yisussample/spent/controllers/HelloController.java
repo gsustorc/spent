eff828076d8a540b998a4f7dd6b5e025237e8172
Casto Colina <castocolina@gmail.com>
2021-04-25 20:59:19 -0400

    feat: Accesing mongo & expose rest controller

--- src/main/java/org/yisussample/spent/controllers/HelloController.java
+++ src/main/java/org/yisussample/spent/controllers/HelloController.java
@@ -1,30 +1,70 @@
 package org.yisussample.spent.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.yisussample.spent.model.Customer;
+import org.yisussample.spent.repository.CustomerRepository;
 
 @RestController("/")
+@Slf4j
 public class HelloController {
 
-    @RequestMapping("/")
-    public String main() {
-        return "Greetings from Spring Boot! Main!";
-    }
+  @Autowired private CustomerRepository repository;
 
-    @RequestMapping("/index")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+  @RequestMapping("/")
+  public String main() {
+    return "Greetings from Spring Boot! Main!";
+  }
 
-    @RequestMapping("/map")
-    public Map<String, Object> map() {
-        return Stream.of(new String[][] {
-                { "Hello", "World" },
-                { "John", "Doe" },
-        }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
-    }
+  @RequestMapping("/index")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+
+  @RequestMapping("/map")
+  public Map<String, Object> map() {
+    return Stream.of(
+            new String[][] {
+              {"Hello", "World"},
+              {"John", "Doe"},
+            })
+        .collect(Collectors.toMap(data -> data[0], data -> data[1]));
+  }
+
+  @RequestMapping(value = "/customer", method = RequestMethod.POST)
+  public Customer save(@RequestBody Customer customer) {
+
+    log.info(" CUSTOMER {}", customer);
+
+    return repository.save(customer);
+  }
+
+  @RequestMapping(value = "/customer", method = RequestMethod.GET)
+  public List<Customer> getAll() {
+    return repository.findAll();
+  }
+
+  @RequestMapping(value = "/customer/{name}", method = RequestMethod.GET)
+  public List<Customer> getByNames(@PathVariable("name") String name) {
+    return repository.findByFirstNameOrLastNameContainingIgnoreCase(name, name);
+  }
+
+  @RequestMapping(value = "/customer/{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable("id") String id) {
+    repository.delete(Customer.builder().id(id).build());
+  }
+
+  @RequestMapping(value = "/customer", method = RequestMethod.DELETE)
+  public void delete() {
+    repository.deleteAll();
+  }
 }
